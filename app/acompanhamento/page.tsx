865b386b4c3bcca9582c0cf2ada9e39dd13df756
Andre Baxinski <andrebaxinski@gmail.com>
2026-08-04 16:10:58 -0300

    Add agency accompaniment workspace

---
+++ app/acompanhamento/page.tsx
@@ -0,0 +1,8 @@
+import { requireAuthenticatedUser } from "@/app/auth";
+import AccompanimentWorkspace from "@/app/components/AccompanimentWorkspace";
+
+export default async function AccompanimentPage() {
+  await requireAuthenticatedUser("/acompanhamento");
+  return <main className="workspace-page"><AccompanimentWorkspace /></main>;
+}
+
